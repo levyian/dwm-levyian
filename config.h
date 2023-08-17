295f5631ba9ca4269b1cfc689131ba7efaa60b5a
levyian <3847164+levyian@users.noreply.github.com>
2023-08-17 00:25:06 -0500

    customize paths

--- config.h
+++ config.h
@@ -27,14 +27,14 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 	"xsetroot", "-cursor_name", "left_ptr", NULL,
-  "kitty", NULL,
-  "flameshot", NULL,
-  "lxpolkit", NULL,
-  "dunst", NULL,
+  	/* "kitty", NULL, */
+  	"flameshot", NULL,
+  	"lxpolkit", NULL,
+  	"dunst", NULL,
 	"picom", NULL,
-  "sh", "-c", "$HOME/dwm-titus/scripts/status", NULL,
-  "sh", "-c", "$HOME/dwm-titus/scripts/dbus", NULL,
-  "feh", "--bg-max", "$HOME/Pictures/background.jpg", NULL,
+  	"sh", "-c", "$HOME/dwm-levyian/scripts/status", NULL,
+  	"sh", "-c", "$HOME/dwm-levyian/scripts/dbus", NULL,
+  	"feh", "--bg-max", "$HOME/Pictures/background.jpg", NULL,
 	NULL /* terminate */
 };
 
@@ -53,7 +53,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-  { "steam",   NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
+  	{ "steam",   NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
